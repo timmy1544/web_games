69855ce5997d5a011ecd5da7fb5d455954f31f5b
Tim Liaw <timliaw@Tims-Laptop.local>
2025-07-16 17:40:58 -0700

    initail setup and add whack-a-mole game

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WhackAMole from './games/whack_a_mole';
+
+function App() {
+  return (
+    <div className="App">
+      <WhackAMole />
+    </div>
+  );
+}
+
+export default App;
